fb86e931401c1663989a97b973ab5213a3dc7ca7
Simon Crowe <simon.r.crowe@protonmail.com>
2024-08-18 20:16:18 +0100

    Half-refactor -- tests need update

--- rightmove_ingester/tests/test_scraping.py
+++ rightmove_ingester/tests/test_scraping.py
@@ -1,9 +1,10 @@
 import pytest
-import scraping
+
+# import scraping
 
 
 @pytest.fixture
-def rightmove_url():
+def to_rent_url():
     return (
         "https://www.rightmove.co.uk/property-to-rent/find.html?"
         "locationIdentifier=REGION%5E87490&"
@@ -18,12 +19,3 @@ def rightmove_url():
         "furnishTypes=unfurnished&"
         "keywords="
     )
-
-
-def test_no_new_results(rightmove_url):
-    def always_not_new(_):
-        return False
-
-    listings = list(scraping.iter_new_listings(rightmove_url, always_not_new))
-
-    assert listings == []
